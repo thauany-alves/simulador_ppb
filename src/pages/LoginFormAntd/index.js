0374ff64f1c9c937a1afd861d6b11f80b5a4ed7a
thauany-alves <thauanyaraujoalves@hotmail.com>
2021-09-22 11:20:16 -0300

    Adding LoginForm from Antd

---
+++ src/pages/LoginFormAntd/index.js
@@ -0,0 +1,53 @@
+import { Form, Input, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import * as S from './styles';
+
+export function LoginFormAntd() {
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <S.Container>
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          name="username"
+          rules={[{ required: true, message: 'Please input your Username!' }]}
+        >
+          <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: 'Please input your Password!' }]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+        </Form.Item>
+
+        <Form.Item>
+        <S.Button type="primary" htmlType="submit" className="login-form-button">
+          Log in
+        </S.Button>
+      </Form.Item>
+      </Form>
+    </S.Container>
+  );
+}
